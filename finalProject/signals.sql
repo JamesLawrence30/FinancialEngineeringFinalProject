ebe6c25289fa475dcfa98790769fe6dc5a739d73
bcd2998 <49543857+bcd2998@users.noreply.github.com>
2019-12-04 23:07:30 -0500

    update

--- finalProject/signals.sql
+++ finalProject/signals.sql
@@ -1,9 +1,17 @@
 --CREATE TABLE Signals (
---    Name CHAR,
---    TimeofAction TIME,
+--    TimeofAction TIME, --> dateAndTime
+--    MACD
+--    RateofChng
+--    Price
 --    BuyOrSell CHAR,
---    Amount DECIMAL
 --);
 
 --SELECT * FROM Signals
 
+--dateAndTime, 
+--gradient --> rate of chnage
+--signlas 
+-- correl: above 0.4 strong, 0.2-0.4 moderate, less 0.2 weak
+-- create table for everything
+--MFST_SIGNAL
+--all 20 - in order seen on python
